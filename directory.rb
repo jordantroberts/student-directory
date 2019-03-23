9d3fa8b0dc3aba191633d070561dd362a0f973d4
Jordan <jordanroberts@talk21.com>
2019-03-23 20:41:21 +0000

    Step 8, exercise 6 - using the center method to make the code neat and aligned

--- directory.rb
+++ directory.rb
@@ -20,20 +20,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-----------"
+  puts "The students of Villains Academy".center(50)
+  puts "-----------".center(50)
 end
 def print(students)
   students.each_with_index do |student, index|
     indexstartat1 = index + 1
       #if student[:name].chr == "J" &&
-      if student[:name].length < 12 
+      if student[:name].length < 12
         puts "#{indexstartat1}. #{student[:name]} #{student[:cohort]} cohort"
       end
   end
 end
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50)
 end
 #nothing happens until we can call the methods
 students = input_students
