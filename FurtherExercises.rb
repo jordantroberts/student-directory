eb53f8cafed16224ab1547e53fc9741de3d71e6f
Jordan <jordanroberts@talk21.com>
2019-03-27 19:26:25 +0000

    Refactoring according to the DRY principle

---
+++ FurtherExercises.rb
@@ -0,0 +1,110 @@
+#After we added the code to load the students from file, we ended up with adding the students to @students in two places.
+#The lines in load_students() and  input_students() are almost the same.
+#This violates the DRY (Don't Repeat Yourself) principle. How can you extract them into a method to fix this problem?
+
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from the students.csv"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+#Extracted to a method 
+def add_students(name)
+  @students << {name: name, cohort: :november}
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    #@students << {name: name, cohort: :november}
+    add_students(name)
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy".center(50)
+  puts "-----------".center(50)
+end
+
+def print_students_list
+  @students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(50)
+end
+
+def save_students
+  file = File.open("students.csv","w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    #@students << {name: name, cohort: :november}
+    add_students(name)
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist."
+    exit
+  end
+end
+
+
+try_load_students
+interactive_menu
